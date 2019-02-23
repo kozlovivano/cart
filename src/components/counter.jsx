d8f015adc952dde4817e0032620b155eeae7ed2c
laguna <lookatme99999@yahoo.com>
2019-02-23 11:52:14 +1000

    Changes

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,19 +3,16 @@ import React, { Component } from 'react';
 
 class Counter extends Component {
     state = {
-        count : 0
+        count : this.props.value
     };
     render() { 
-
-        let classes = this.getBadgeClass();
-
         return (
-          <React.Fragment>
-            <span className={classes}>{this.formatCount()}</span>
-            <button className="btn btn-secondary btn-sm">
-              Increment
-            </button>
-          </React.Fragment>
+            <div>
+                <span className={this.getBadgeClass()}>{this.formatCount()}</span>
+                <button onClick={() => this.handleIncreament()} className="btn btn-secondary btn-sm">
+                    Increment
+                </button>
+            </div>
         );
     }
 
@@ -30,8 +27,8 @@ class Counter extends Component {
         return count === 0 ? 'Zero' : count;
     }
 
-    increaseCount(){
-
+    handleIncreament = () => {
+        this.setState({count: this.state.count + 1});
     }
 }
  
